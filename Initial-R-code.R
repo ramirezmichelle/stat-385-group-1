2c61ce0b839b6e6da548f82481da04c4516c00b0
Michelle Ramirez <mramir71@uic.edu>
2022-04-12 11:03:52 -0500

    Added comments

--- Initial-R-code.R
+++ Initial-R-code.R
@@ -1,8 +1,11 @@
 setwd("/Users/michelleramirez/Documents/STAT_385/final-project")
 
+#splitting data into training and test
 TravelInsurance <- read.csv(file="TravelInsurancePrediction.csv",header=TRUE)
 set.seed(2022)
 Training <- sample(c(1:1987),1500,replace=FALSE)
 Test <- c(1:1987)[-Training]
 TrainData <- TravelInsurance[Training,]
 TestData <- TravelInsurance[Test,]
+
+print(length(Training))
