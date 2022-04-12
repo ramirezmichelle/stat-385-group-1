a9683285563567766274be6c2b0403a202008c0a
Michelle Ramirez <51290254+ramirezmichelle@users.noreply.github.com>
2022-04-12 09:47:53 -0500

    Add files via upload

---
+++ Initial-R-code.R
@@ -0,0 +1,8 @@
+setwd("/Users/michelleramirez/Documents/STAT_385/final-project")
+
+TravelInsurance <- read.csv(file="TravelInsurancePrediction.csv",header=TRUE)
+set.seed(2022)
+Training <- sample(c(1:1987),1500,replace=FALSE)
+Test <- c(1:1987)[-Training]
+TrainData <- TravelInsurance[Training,]
+TestData <- TravelInsurance[Test,]
